859ee5a1e56cfbcf898c24cd7b07662d09cc68f8
oriolagobat <oriolagobat@gmail.com>
2022-04-15 21:18:10 +0200

    Implemented new game button on configuration screen that I forgot

--- app/src/main/java/com/example/filler/gui/NewGameConfiguration.kt
+++ app/src/main/java/com/example/filler/gui/NewGameConfiguration.kt
@@ -3,10 +3,17 @@ package com.example.filler.gui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filler.R
+import com.example.filler.databinding.ActivityNewGameConfigurationBinding
 
 class NewGameConfiguration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game_configuration)
+
+        // Set this class binding
+        val binding = ActivityNewGameConfigurationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
