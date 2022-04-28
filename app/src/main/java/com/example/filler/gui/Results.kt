7c09ee300810b8f60132df9164987d0fd7d781df
oriolagobat <oriolagobat@gmail.com>
2022-04-28 23:19:01 +0200

    Adds mediaPlayer sounds on the result screen

--- app/src/main/java/com/example/filler/gui/Results.kt
+++ app/src/main/java/com/example/filler/gui/Results.kt
@@ -1,7 +1,8 @@
 package com.example.filler.gui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.filler.R
 import com.example.filler.databinding.ActivityResultsDrawBinding
 import com.example.filler.databinding.ActivityResultsLoseBinding
@@ -10,14 +11,25 @@ import com.example.filler.databinding.ActivityResultsWinBinding
 class Results : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val resultType = intent.getStringExtra("resultType")
 
         // Inflate the view corresponding to the outcome of the game
-        val binding = when (intent.getStringExtra("resultType")) {
+        val binding = when (resultType) {
             "win" -> ActivityResultsWinBinding.inflate(layoutInflater)
             "loose" -> ActivityResultsLoseBinding.inflate(layoutInflater)
             "draw" -> ActivityResultsDrawBinding.inflate(layoutInflater)
             else -> throw IllegalArgumentException("No more possible results")
         }
         setContentView(binding.root)
+
+        val playerIntent = Intent(this, SongPlayer::class.java)
+        when (resultType) {
+            "win" -> playerIntent.putExtra("song", R.raw.win)
+            "loose" -> playerIntent.putExtra("song", R.raw.lose)
+            "draw" -> playerIntent.putExtra("song", R.raw.draw)
+            else -> throw IllegalArgumentException("No more possible results")
+        }
+        stopService(playerIntent)
+        startService(playerIntent)
     }
 }
---
+++ app/src/main/java/com/example/filler/gui/SongPlayer.kt
@@ -0,0 +1,44 @@
+package com.example.filler.gui
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class SongPlayer : Service() {
+    private lateinit var player: MediaPlayer
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+
+        val song = intent?.getIntExtra("song", -1)
+
+        manageSoundPlayer(song!!)
+        return startId
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (player.isPlaying) {
+            player.stop()
+            player.release()
+        }
+    }
+
+    private fun manageSoundPlayer(song: Int) {
+        if (!::player.isInitialized || player.isPlaying) {  // First execution
+            player = MediaPlayer.create(applicationContext, song)
+            player.isLooping = true  // FIXME: Remove, for testing only
+            player.start()
+        } else {  // Other executions, stop and re-set taskGPlayer
+            player.stop()
+            player.reset()
+            player.release()
+            player = MediaPlayer.create(applicationContext, song)
+            player.isLooping = true  // FIXME: Remove, for testing only
+            player.start()
+        }
+    }
+}
