56c239234a066c9788b3af61eab245509ce77933
oriolagobat <oriolagobat@gmail.com>
2022-04-29 13:17:52 +0200

    Develops result layout. Also added different layouts settings (main header and image) that change depending on the outcome of the game.

--- app/src/main/java/com/example/filler/gui/Results.kt
+++ app/src/main/java/com/example/filler/gui/Results.kt
@@ -7,28 +7,49 @@ import com.example.filler.R
 import com.example.filler.databinding.ActivityResultsBinding
 
 class Results : AppCompatActivity() {
+    private lateinit var binding: ActivityResultsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityResultsBinding = ActivityResultsBinding.inflate(layoutInflater)
+        binding = ActivityResultsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val resultType = intent.getStringExtra("resultType")
 
+        // Start the media player with the sound corresponding to the outcome of the game
+        manageSongPlayer()
+
+        // Set the corresponding layout image and text corresponding to the outcome of the game
+
         when (resultType) {
-            "win" -> binding.textView.text = "win"
-            "loose" -> binding.textView.text = "lose"
-            "draw" -> binding.textView.text = "draw"
+            "win" -> manageWinOutcome()
+            "loose" -> manageLoseOutcome()
+            "draw" -> manageDrawOutcome()
             else -> throw IllegalArgumentException("No more possible results")
         }
-        setContentView(binding.root)
+    }
 
+    private fun manageSongPlayer() {
         val playerIntent = Intent(this, SongPlayer::class.java)
-        when (resultType) {
+        when (intent.getStringExtra("resultType")) {
             "win" -> playerIntent.putExtra("song", R.raw.win)
             "loose" -> playerIntent.putExtra("song", R.raw.lose)
             "draw" -> playerIntent.putExtra("song", R.raw.draw)
             else -> throw IllegalArgumentException("No more possible results")
         }
-        stopService(playerIntent)
         startService(playerIntent)
+    }
+
+    private fun manageWinOutcome() {
+        binding.outcomeImage.setImageResource(R.drawable.result_win)
+        binding.outcomeHeader.text = getString(R.string.results_win_header)
+    }
+
+    private fun manageLoseOutcome() {
+        binding.outcomeImage.setImageResource(R.drawable.result_lose)
+        binding.outcomeHeader.text = getString(R.string.results_lose_header)
+    }
+
+    private fun manageDrawOutcome() {
+        binding.outcomeImage.setImageResource(R.drawable.result_draw)
+        binding.outcomeHeader.text = getString(R.string.results_draw_header)
     }
 }
