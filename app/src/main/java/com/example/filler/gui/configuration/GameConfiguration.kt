1e780782337dfb7189d74587a2e0ff5fe6645896
oriolagobat <oriolagobat@gmail.com>
2022-05-05 17:31:52 +0200

    Adds logger info from the configuration Fixes #36.

--- app/src/main/java/com/example/filler/gui/configuration/GameConfiguration.kt
+++ app/src/main/java/com/example/filler/gui/configuration/GameConfiguration.kt
@@ -5,8 +5,9 @@ import android.widget.AdapterView
 import android.widget.RadioButton
 import com.example.filler.R
 import com.example.filler.databinding.ActivityNewGameConfigurationBinding
-import com.example.filler.gui.shared.closeKeyboardClearFocus
 import com.example.filler.gui.configuration.data.*
+import com.example.filler.gui.shared.closeKeyboardClearFocus
+import com.example.filler.log.Logger
 import java.io.Serializable
 
 data class GameConfiguration(
@@ -17,17 +18,25 @@ data class GameConfiguration(
     val difficultyString: DifficultyString = DifficultyString()
 ) : Serializable {
     fun spinnerClick(spinner: AdapterView<*>?, position: Int) {
-        // Don't ignore first call, it will serve as the default if user doesn't select anything
         when (spinner?.id) {
-            R.id.colorSpinner -> colorNumber.value = getItemFromSpinner(spinner, position)
-            R.id.gridSpinner -> boardSize.value = getItemFromSpinner(spinner, position)
+            R.id.colorSpinner -> colorSpinnerClick(spinner, position)
+            R.id.boardSpinner -> boardSizeClick(spinner, position)
         }
     }
 
-    private fun getItemFromSpinner(spinner: AdapterView<*>, position: Int): String =
-        spinner.getItemAtPosition(position).toString()
+    private fun colorSpinnerClick(spinner: AdapterView<*>?, position: Int) {
+        colorNumber.value = getItemFromSpinner(spinner, position)
+        Logger.logI("User chose color number: ${colorNumber.value}")
+    }
 
-    // Manages clicks on the editText, checkBox and button
+    private fun boardSizeClick(spinner: AdapterView<*>?, position: Int) {
+        boardSize.value = getItemFromSpinner(spinner, position)
+        Logger.logI("User chose board size: ${boardSize.value}")
+    }
+
+    private fun getItemFromSpinner(spinner: AdapterView<*>?, position: Int): String =
+        spinner?.getItemAtPosition(position).toString()
+
     fun click(
         v: View?,
         context: NewGameConfiguration,
@@ -40,7 +49,6 @@ data class GameConfiguration(
         }
     }
 
-    // Manages the username input EditText
     private fun manageUsernameInput(
         context: NewGameConfiguration,
         binding: ActivityNewGameConfigurationBinding
@@ -50,24 +58,26 @@ data class GameConfiguration(
             usernameInput.error = "Please enter a username"
         } else {
             username.value = usernameInput.text.toString()
+            Logger.logI("User chose username: ${username.value}")
         }
         closeKeyboardClearFocus(context, usernameInput)
     }
 
-    // Manages time checkBox
     private fun manageTimeCheckBox(binding: ActivityNewGameConfigurationBinding) {
         this.timeControl.value = binding.timeCheckBox.isChecked
-    }
-
-    // Manages new game button
-    private fun manageNewGameButton(context: NewGameConfiguration) {
-        if (correctGameSettings(context, username, difficultyString)) {
-            StartNewGame(this, context)
-        }
+        Logger.logI("User chose time control: ${timeControl.value}")
     }
 
     fun radioButtonClick(context: NewGameConfiguration, checkedId: Int) {
         val checked: RadioButton = context.findViewById(checkedId)
         this.difficultyString.value = checked.text.toString()
+        Logger.logI("User chose difficulty: ${difficultyString.value}")
+    }
+
+    private fun manageNewGameButton(context: NewGameConfiguration) {
+        if (correctGameSettings(context, username, difficultyString)) {
+            StartNewGame(this, context)
+            Logger.logI("User chooses to start a new game")
+        }
     }
 }
