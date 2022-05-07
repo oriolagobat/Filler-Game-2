5ccff67b2f5472b0c067333d68f4e250ce3b334e
FerranAD <farandomingo@gmail.com>
2022-05-07 13:22:40 +0200

    Adds board filler test

--- app/src/test/java/com/example/filler/logic/boardfiller/FillTest.kt
+++ app/src/test/java/com/example/filler/logic/boardfiller/FillTest.kt
@@ -3,9 +3,10 @@ package com.example.filler.logic.boardfiller
 import com.example.filler.constants.logic.GameColor
 import com.example.filler.logic.board.BoardColorInitializer
 import com.example.filler.logic.board.BoardImpl
+import com.example.filler.logic.board.Position
 import com.example.filler.logic.colors.RandomColorGenerator
-import org.junit.Assert.*
-
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -17,14 +18,31 @@ class FillTest {
 
     @Before
     fun setUp() {
-
+        boardColorFiller.start()
     }
 
     @Test
-    fun fill() {
-        boardColorFiller.start()
+    fun `Home positions have different colors`() {
         val p1HomeColor = board.getColor(board.getP1Home())
         val p2HomeColor = board.getColor(board.getP2Home())
         assertNotEquals(p1HomeColor, p2HomeColor)
     }
+
+    // Test if there are no two equal neighbor colors
+    @Test
+    fun `Neighbor positions have different colors`() {
+        boardColorFiller.start()
+        val res1 = (0 until board.getNumCells()).toMutableList()
+            .map { index -> board.indexToPosition(index) }
+            .any { position ->
+                position.getSurroundingPositions().any { neighbor ->
+                    board.hasPosition(neighbor) && shareColors(position, neighbor)
+                }
+            }
+        assertFalse(res1)
+    }
+
+    private fun shareColors(position: Position, neighbor: Position): Boolean =
+        board.getColor(position) == board.getColor(neighbor)
+
 }
