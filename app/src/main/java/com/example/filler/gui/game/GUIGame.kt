5842fa0dff092d6c5ef7d082e44d19a79ce04da3
oriolagobat <oriolagobat@gmail.com>
2022-05-21 11:06:22 +0200

    Remove back button action on game screen

--- app/src/main/java/com/example/filler/gui/game/GUIGame.kt
+++ app/src/main/java/com/example/filler/gui/game/GUIGame.kt
@@ -1,11 +1,8 @@
 package com.example.filler.gui.game
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.fragment.app.FragmentActivity
-import com.example.filler.R
 import com.example.filler.databinding.ActivityGameBinding
-import com.example.filler.gui.game.fragments.GameFragment
 
 class GUIGame : FragmentActivity() {
 
@@ -15,8 +12,9 @@ class GUIGame : FragmentActivity() {
 
         val binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        val gameFrag = binding.gameFragment
-//        val gameFrag = supportFragmentManager
-//            .findFragmentById(R.id.gameFragment) as GameFragment?
+    }
+
+    override fun onBackPressed() {
+        return
     }
 }
