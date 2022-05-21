e6f0c461157031ee0fce2d58d33f176906e6b5d8
FerranAD <farandomingo@gmail.com>
2022-05-21 17:14:54 +0200

    feat(preferences-fragment): adds basic preferences fragment
    This partially fixes #9.

---
+++ app/src/main/java/com/example/filler/gui/preferences/PreferencesFragment.kt
@@ -0,0 +1,16 @@
+package com.example.filler.gui.preferences
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+import com.example.filler.R
+
+
+class PreferencesFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
