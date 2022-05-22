fc1d695fc849386754cfa06be8efc54d0fae4a43
oriolagobat <oriolagobat@gmail.com>
2022-05-22 18:03:54 +0200

    Fixes image bug where game images weren't shown also in landscape orientation

--- app/src/main/java/com/example/filler/gui/game/fragments/GameFragment.kt
+++ app/src/main/java/com/example/filler/gui/game/fragments/GameFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.filler.R
 import com.example.filler.constants.gui.*
 import com.example.filler.constants.logic.Difficulty
 import com.example.filler.constants.logic.GameState
