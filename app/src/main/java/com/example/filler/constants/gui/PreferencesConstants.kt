3c7ed1f7ba18a0033408cec009ac252b96584484
FerranAD <farandomingo@gmail.com>
2022-05-22 13:14:03 +0200

    feat(preferences-fragment): adds all but profile pic preferences
    This partially fixes #9.

---
+++ app/src/main/java/com/example/filler/constants/gui/PreferencesConstants.kt
@@ -0,0 +1,32 @@
+package com.example.filler.constants.gui
+
+// Colors
+const val NUM_COLORS_KEY = "numColors"
+const val NUM_COLORS_DEFAULT = 8
+const val NUM_COLORS_MIN = 3
+const val NUM_COLORS_MAX = 8
+
+// Board size
+const val BOARD_SIZE_KEY = "boardSize"
+const val BOARD_SIZE_DEFAULT = 6
+const val BOARD_SIZE_MIN = 3
+const val BOARD_SIZE_MAX = 9
+
+// Difficulty
+const val DIFFICULTY_KEY = "difficulty"
+const val DIFFICULTY_DEFAULT = "medium"
+const val DIFFICULTY_EASY = "easy"
+const val DIFFICULTY_MEDIUM = "medium"
+const val DIFFICULTY_HARD = "hard"
+
+// Time control
+const val TIME_CONTROL_KEY = "timeControl"
+const val TIME_CONTROL_DEFAULT = false
+
+// Alias
+const val ALIAS_KEY = "alias"
+const val ALIAS_DEFAULT = "Player"
+
+// Music
+const val MUSIC_KEY = "music"
+const val MUSIC_DEFAULT = true
--- app/src/main/java/com/example/filler/gui/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/example/filler/gui/preferences/PreferencesFragment.kt
@@ -1,17 +1,137 @@
 /*
 package com.example.filler.gui.preferences
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.preference.PreferenceFragmentCompat
-import com.example.filler.R
+import androidx.preference.*
+import com.example.filler.constants.gui.*
 
+//TODO: Replace with call to arrays on resources.
+val NUM_COLORS_ENTRIES = arrayOf("3", "4", "5", "6", "7", "8")
+val BOARD_SIZE_ENTRIES = arrayOf("3 x 3", "4 x 4", "5 x 5", "6 x 6", "7 x 7", "8 x 8", "9 x 9")
+val DIFFICULTY_ENTRIES = arrayOf("Easy", "Medium", "Hard")
 
 class PreferencesFragment : PreferenceFragmentCompat() {
+    private lateinit var screen: PreferenceScreen;
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        initPreferenceScreen()
+        buildPreferenceScreen(screen.context)
+        preferenceScreen = screen
+    }
+
+    private fun initPreferenceScreen() {
+        val context = preferenceManager.context
+        screen = preferenceManager.createPreferenceScreen(context)
+    }
+
+    private fun buildPreferenceScreen(context: Context) {
+        addGameCategoryPreferences(context)
+        addGeneralCategoryPreferences(context)
+    }
+
+    private fun addGameCategoryPreferences(context: Context) {
+        addGameCategory(context)
+        addBoardSizePreference(context)
+        addNumColorsPreference(context)
+        addDifficultyPreference(context)
+        addTimeControlPreference(context)
+    }
+
+    private fun addGameCategory(context: Context) {
+        PreferenceCategory(context).apply {
+            title = "Game"
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addBoardSizePreference(context: Context) {
+        ListPreference(context).apply {
+            key = BOARD_SIZE_KEY
+            title = "Board size"
+            summary = "Choose the size of the board"
+            entries = BOARD_SIZE_ENTRIES
+            entryValues = rangetoCharArray(BOARD_SIZE_MIN, BOARD_SIZE_MAX)
+            setDefaultValue(BOARD_SIZE_DEFAULT.toString())
+            screen.addPreference(this)
+        }
+    }
+
+    // Returns an array of numeral characters from min to max
+    private fun rangetoCharArray(minValue: Int, maxValue: Int): Array<out CharSequence> {
+        return Array(maxValue - minValue + 1) { "" }.apply {
+            for (i in minValue..maxValue)
+                this[i - minValue] = i.toString()
+        }
+    }
+
+    private fun addNumColorsPreference(context: Context) {
+        ListPreference(context).apply {
+            key = NUM_COLORS_KEY
+            title = "Colors"
+            summary = "Choose the number of colors"
+            entries = NUM_COLORS_ENTRIES
+            entryValues = rangetoCharArray(NUM_COLORS_MIN, NUM_COLORS_MAX)
+            this.setDefaultValue(NUM_COLORS_DEFAULT.toString())
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addDifficultyPreference(context: Context) {
+        ListPreference(context).apply {
+            key = DIFFICULTY_KEY
+            title = "Difficulty"
+            summary = "Choose the difficulty"
+            entries = DIFFICULTY_ENTRIES
+            entryValues = arrayOf(DIFFICULTY_EASY, DIFFICULTY_MEDIUM, DIFFICULTY_HARD)
+            this.setDefaultValue(DIFFICULTY_DEFAULT)
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addTimeControlPreference(context: Context) {
+        SwitchPreferenceCompat(context).apply {
+            key = TIME_CONTROL_KEY
+            title = "Time Control"
+            summaryOn = "Time Control On"
+            summaryOff = "Time Control Off"
+            isChecked = TIME_CONTROL_DEFAULT
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addGeneralCategoryPreferences(context: Context) {
+        addGeneralCategory(context)
+        addAliasPreference(context)
+//        addProfilePicPreference(context)
+        addMusicPreference(context)
+    }
+
+    private fun addGeneralCategory(context: Context) {
+        PreferenceCategory(context).apply {
+            title = "General"
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addAliasPreference(context: Context) {
+        EditTextPreference(context).apply {
+            key = ALIAS_KEY
+            title = "Alias"
+            summary = "Choose your alias"
+            this.setDefaultValue(ALIAS_DEFAULT)
+            screen.addPreference(this)
+        }
+    }
+
+    private fun addMusicPreference(context: Context) {
+        SwitchPreferenceCompat(context).apply {
+            key = MUSIC_KEY
+            title = "Music"
+            summaryOn = "Music On"
+            summaryOff = "Music Off"
+            isChecked = MUSIC_DEFAULT
+            screen.addPreference(this)
+        }
     }
 }*/
