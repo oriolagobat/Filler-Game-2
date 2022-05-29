acca698395eff362f8caa10d6486a67d5e7aa3e4
FerranAD <farandomingo@gmail.com>
2022-05-29 11:42:14 +0200

    style(persistence): add style to summary detail

--- app/src/main/java/com/example/filler/persistence/RegFrag.kt
+++ app/src/main/java/com/example/filler/persistence/RegFrag.kt
@@ -36,9 +36,9 @@ class RegFrag : Fragment() {
     }
 
     private fun setDetailView(current: GameSummary?) {
-        binding.textview.text = "Lorem ipsum dolor sit amet, consectetur " +
-                "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//        binding.textview.text = "Lorem ipsum dolor sit amet, consectetur " +
+//                "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+//                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 //        binding.textview.text = current?.endTime ?: "No current game"
     }
 }
